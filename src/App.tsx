63ba98d3b30d7b3e22662aa03d0ccd2cd6dc7cc7
Joosung Park <admin@slowmotion.dev>
2019-08-30 20:01:50 +0900

    add: Keypad

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import './App.css';
-import '@material/react-button/dist/button.css';
-import {Button} from '@material/react-button';
+import {Keypad} from './components/Keypad';
 
 class App extends React.Component {
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
             <div className="App">
-                <Button raised style={{
-                    height: '100px',
-                    width: '100px',
-                }} onClick={() => {
-                    console.log('Hello!');
-                }} disabled={false}>
-                    Hello!
-                </Button>
+                <Keypad onEnter={console.log}/>
             </div>
         );
     }
---
+++ src/components/Keypad.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import '@material/react-button/dist/button.css';
+import './Keypad.css';
+import {Button} from '@material/react-button';
+import {Backspace, Clear, Done} from '@material-ui/icons';
+
+interface IKeypadProp {
+    onEnter: (value: number) => void,
+}
+
+interface IKeypadState {
+    value: number,
+}
+
+export class Keypad extends React.Component<IKeypadProp, IKeypadState> {
+    private readonly text = [
+        <Clear/>,
+        '',
+        '',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        <Backspace/>,
+        '0',
+        <Done/>,
+    ];
+
+    constructor(prop: IKeypadProp) {
+        super(prop);
+        this.state = {
+            value: 0,
+        };
+    }
+
+    private clear() {
+        console.log('Clear');
+        this.setState({value: 0});
+    }
+
+    private backspace() {
+        console.log('Backspace');
+        this.setState({value: Math.floor(this.state.value / 10)});
+    }
+
+    private number(n: number) {
+        console.log(`Number: ${n}`);
+        this.setState({value: this.state.value * 10 + n});
+    }
+
+    handler(id: number) {
+        switch (id) {
+            case 0:
+                this.clear();
+                break;
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                this.number(id - 2);
+                break;
+            case 13:
+                this.number(0);
+                break;
+            case 12:
+                this.backspace();
+                break;
+        }
+    }
+
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        return (
+            <div className={'keypad'}>
+                <div className={'keypad-value'}>{this.state.value}</div>
+                <div className={'keypad-wrap'}>
+                    {this.text.map((e, i) => {
+                        return (
+                            <Button raised className={'keypad-key'} style={{
+                                gridArea: `${Math.floor((i / 3) + 2).toString()} / ${i % 3 + 1}`,
+                            }} onClick={() => {
+                                this.handler(i);
+                            }} key={i}>{e}</Button>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
